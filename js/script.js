98ec18ee9917732f053c222bbb5215c6870aabd4
minasanta <minasanta77@gmail.com>
2022-09-28 23:29:14 +0200

    Add Animated Width on Scrolling on Section Skills

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 // Main
+const ourskillsSection = document.getElementById("our-skills");
+const progressSpans = [...document.querySelectorAll("#our-skills .skill .the-progress span")];
 const statsSpans = [...document.querySelectorAll("#stats .box span.number")];
 const statsSection = document.getElementById("stats");
 let startWindowScrollFunctionOnCounter = false;
 
 window.onscroll = function () {
+    if (ourskillsSection.offsetTop <= window.scrollY) {
+        progressSpans.forEach(span => {
+            span.style.width = span.dataset.width;
+        });
+    }
     if (window.scrollY >= statsSection.offsetTop) {
         if (!startWindowScrollFunctionOnCounter) {
             statsSpans.forEach(span => increasNumberToAchieveGoals(span));
